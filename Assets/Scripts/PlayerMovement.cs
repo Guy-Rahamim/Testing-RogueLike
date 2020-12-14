ed2d2732d9325534564d3384cbb2b82b46410f95
Etzionid <etzionid@gmail.com>
2020-12-14 19:32:11 +0200

    Fmod integration

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,30 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Camera camera;
     [SerializeField] bool followMouse=false;
     [SerializeField] float speedModifier = 3f;
-   
+    FMODUnity.StudioEventEmitter footsteps;
+    float walkDelay = 0.55f;
+    float lastWalk = 0;
+
+    [FMODUnity.EventRef]
+    bool isWalking;
+
     Vector2 movement;
 
     float horizontal;
     float vertical;
+    public static Vector2 soundMovement;
 
     void Start()
     {
+        footsteps= GetComponent<FMODUnity.StudioEventEmitter>();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
     void Update()
     {
+        playFootsSteps();
+
+        soundMovement = new Vector2(horizontal, vertical);
         Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
         animator.SetFloat("horizontal", mousePos.x);
         animator.SetFloat("vertical", mousePos.y);
@@ -39,5 +50,13 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * speedModifier*Time.fixedDeltaTime);
 
     }
-      
+        
+    void playFootsSteps()
+    {
+        if ((Time.realtimeSinceStartup - lastWalk > walkDelay ) && movement.magnitude>0)
+        {
+            footsteps.Play();
+            lastWalk = Time.realtimeSinceStartup;
+        }
+    }
 }
